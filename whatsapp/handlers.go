18e84d22908676112a2c6e0a31f3cf1b52d5f5e4
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2025-08-05 16:34:58 +0000

    fix(handler): I don't like buttons under media

--- whatsapp/handlers.go
+++ whatsapp/handlers.go
@@ -974,7 +974,6 @@ func MessageFromOthersEventHandler(text string, v *events.Message, isEdited bool
 						MessageId: replyToMsgId,
 					},
 					MessageThreadId: threadId,
-					ReplyMarkup:     replyMarkup,
 				})
 				if sentMsg.MessageId != 0 {
 					database.MsgIdAddNewPair(msgId, v.Info.MessageSource.Sender.String(), v.Info.Chat.String(),
@@ -989,7 +988,6 @@ func MessageFromOthersEventHandler(text string, v *events.Message, isEdited bool
 					MessageId: replyToMsgId,
 				},
 				MessageThreadId: threadId,
-				ReplyMarkup:     replyMarkup,
 			})
 			if sentMsg.MessageId != 0 {
 				database.MsgIdAddNewPair(msgId, v.Info.MessageSource.Sender.String(), v.Info.Chat.String(),
